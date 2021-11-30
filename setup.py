b2d3158bb79dffaed1585f7b76397c44a6915a06
Bruno Carlos Vieira <45644699+brunocvs7@users.noreply.github.com>
2021-11-30 06:30:31 -0300

    uploading repo structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='utils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My Utils',
+    author='Bruno Vieira',
+    license='MIT',
+)
