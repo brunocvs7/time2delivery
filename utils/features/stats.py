0e9607f213262fe31e9606007790935f6eb56a07
Bruno Carlos Vieira <45644699+brunocvs7@users.noreply.github.com>
2021-12-05 22:42:21 -0300

    finished eda and feature selection

--- utils/features/stats.py
+++ utils/features/stats.py
@@ -2,27 +2,49 @@ from numpy.lib.arraysetops import unique
 import pandas as pd
 from scipy.stats import mstats
 
-def check_kruskal_wallis(df:pd.DataFrame, x:str, y:str, alpha:float=0.05):
+def check_kruskal_wallis(df:pd.DataFrame, cat_columns:list, target:str, alpha:float=0.05):
     """Applies Kruskal-Wallis test on a list of arrays
 
     Args:
         df (pd.DataFrame): A pandas dataframe with the original data
         x (str): Name of the column contaning the groups to be tested
         y (str): Name of the column with the characteristic to be tested
-        alpha (float, optional): Significante level to reject Null hypothesis. Defaults to 0.05.
+        alpha (float, optional): Significance level to reject Null hypothesis. Defaults to 0.05.
+    Returns:
+        kw_result: A dictionary, in which keys are name of columns and values are flags indicating if the feature passed the test or not.
     """
-    unique_groups = df[x].unique()
-    list_arrays = []
+    kw_result = dict()
+    for col in cat_columns:
+        unique_groups = df[col].unique()
+        list_arrays = []
 
-    for i in unique_groups:
-        list_arrays.append(df[df[x]==i][y].values)
-    H, pvalue = mstats.kruskalwallis(*list_arrays)
-    print("H-statistic:", H)
-    print("P-Value:", pvalue)
+        for i in unique_groups:
+            list_arrays.append(df[df[col]==i][target].values)
+        H, pvalue = mstats.kruskalwallis(*list_arrays)
+        if pvalue < alpha:
+            kw_result[col] = 'reject'
+        if pvalue > alpha:
+            kw_result[col] = 'not reject'
+    return kw_result
 
-    if pvalue < alpha:
-        print("Reject NULL hypothesis - Significant differences exist between groups.")
-    if pvalue > alpha:
-        print("Accept NULL hypothesis - No significant difference between groups.")
-        
+def check_spearman_correlation(df:pd.DataFrame, num_columns:list, target, alpha:float=0.05):
+    """Performs spearman correlation test
+
+    Args:
+        df (pd.DataFrame): A dataframe containing features and target
+        num_columns (list): List of numerical columns to be tested
+        target ([type]): Name of the target column
+        alpha (float, optional): Significance level for the test. Defaults to 0.05.
+
+    Returns:
+        sp_result: A dictionary, in which keys are name of columns and values are flags indicating if the feature passed the test or not.
+    """
+    sp_result = dict()
+    for col in num_columns:
+        correlation, pvalue = mstats.spearmanr(df[col], df[target])
+        if pvalue < alpha:
+            sp_result[col] = 'reject'
+        if pvalue > alpha:
+           sp_result[col] = 'not reject'
+    return sp_result
 
